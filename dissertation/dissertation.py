aad29c8199c218b89ef8d816ae40578bb968928e
H. Chase Stevens <chase@chasestevens.com>
2014-07-09 13:30:25 +0100

    Add support for neighborhoods

--- dissertation/dissertation.py
+++ dissertation/dissertation.py
@@ -1,30 +1,34 @@
 from swarm import Swarm
 from matplotlib import pyplot as pl
 from matplotlib import animation
+from math import sqrt, sin, cos
 
 
 def update_plot(i, swarm, plot, fitness_func):
     print i, '\b' * (len(str(i)) + 2),
-    plot.set_offsets(zip([particle._position for particle in swarm.step(fitness_func)]))
+    plot.set_offsets(zip([particle.position for particle in swarm.step(fitness_func)]))
     return plot,
 
 
 if __name__ == '__main__':
     iterations = 1000
+    dimensions = 2
     save = True
 
     fig = pl.figure()
-    pl.axis([0, 1, 0, 1])
-    
-    swarm = Swarm(2, 150)
-    fitness_func = lambda (x, y): -abs(((x * 10) ** 2) - (y * 10)) - (100 if x <= 0 else 0)
+    pl.axis([0, 1,] * dimensions)
 
-    plot = pl.scatter(*zip(*[particle._position for particle in swarm.step(fitness_func)]))
+    swarm = Swarm(dimensions, 150)
+    fitness_func = lambda (x, y): -abs(((x * 10) ** 1.5) - (y * 10)) - (100 if x <= 0 else 0)
+    fitness_func = lambda (x, y): -sqrt((0.5 - x) ** 2 + (0.5 - y) ** 2)
+    fitness_func = lambda (x, y): sin(10 * x) + cos(10 * y)
+
+    plot = pl.scatter(*zip(*[particle.position for particle in swarm.step(fitness_func)]))
 
     anim = animation.FuncAnimation(fig, update_plot, frames=xrange(iterations), fargs=(swarm, plot, fitness_func))
 
     pl.show()
 
-    final = swarm.get_best_position(fitness_func)
+    final = swarm.get_best_position_coords(fitness_func)
     print final, fitness_func(final)
     raw_input()
--- dissertation/swarm.py
+++ dissertation/swarm.py
@@ -2,14 +2,14 @@ import random
 import numpy
 import collections
 import operator
-from matplotlib import pyplot as pl
+import itertools
 
 
 class Particle(object):
 
     MAX_VELOCITY = 1
     MAX_POSITION = 10
-    INERTIAL_DAMPENING_SCHEDULE = lambda __, inertia: inertia / 1.001
+    INERTIAL_DAMPENING = 1.001
 
     Position = collections.namedtuple('Position', 'fitness position')
 
@@ -18,63 +18,96 @@ class Particle(object):
         self._cognitive_comp = cognitive_comp
         self._social_comp = social_comp
 
-        self._position = numpy.array([random.random() for param in xrange(no_params)])
-        self._best_position = self._position.copy()
+        self.position = numpy.array([random.random() for param in xrange(no_params)])
+        self._best_position = self.position.copy()
         self._best_fitness = float('-inf')
-        self._velocity = numpy.array([random.uniform(-self.MAX_VELOCITY, self.MAX_VELOCITY) 
-                                      for param in 
+        self._velocity = numpy.array([random.uniform(-self.MAX_VELOCITY, self.MAX_VELOCITY)
+                                      for param in
                                       xrange(no_params)
                                       ]
                                      )
+
+    @staticmethod
+    def _inertial_dampening_schedule(inertia):
+        return inertia / Particle.INERTIAL_DAMPENING
 
     def update(self, best_neighbor_position):
         cognitive_mod = random.random()
         social_mod = random.random()
 
-        self._inertia = self.INERTIAL_DAMPENING_SCHEDULE(self._inertia)
+        self._inertia = self._inertial_dampening_schedule(self._inertia)
 
         inertial_velocity = self._inertia * self._velocity
-        cognitive_velocity = self._cognitive_comp * cognitive_mod * (self._best_position - self._position)
-        social_velocity = self._social_comp * social_mod * (best_neighbor_position - self._position)
+        cognitive_velocity = self._cognitive_comp * cognitive_mod * (self._best_position - self.position)
+        social_velocity = self._social_comp * social_mod * (best_neighbor_position - self.position)
 
         self._velocity = inertial_velocity + cognitive_velocity + social_velocity
 
         self._velocity = numpy.maximum(self._velocity, [-self.MAX_VELOCITY] * len(self._velocity))
         self._velocity = numpy.minimum(self._velocity, [self.MAX_VELOCITY] * len(self._velocity))
 
-        self._position += self._velocity
+        self.position += self._velocity
 
-        self._position = numpy.maximum(self._position, [-self.MAX_POSITION] * len(self._position))
-        self._position = numpy.minimum(self._position, [self.MAX_POSITION] * len(self._position))
+        self.position = numpy.maximum(self.position, [-self.MAX_POSITION] * len(self.position))
+        self.position = numpy.minimum(self.position, [self.MAX_POSITION] * len(self.position))
 
     def best_position(self, fitness_function):
-        if self._position.max() <= 1 and self._position.min() >= 0: # Engelbrecht 2005
-            fitness = fitness_function(self._position)
+        if self.position.max() <= 1 and self.position.min() >= 0: # Engelbrecht 2005
+            fitness = fitness_function(self.position)
             if self._best_fitness < fitness:
                 self._best_fitness = fitness
-                self._best_position = self._position.copy()
+                self._best_position = self.position.copy()
         return self.Position(fitness=self._best_fitness, position=self._best_position)
 
 
 class Swarm(object):
 
-    def __init__(self, no_params, no_particles, inertia=1.2, cognitive_comp=2, social_comp=2): # Shi & Eberhart 1998
-        self.particles = [Particle(no_params, inertia, cognitive_comp, social_comp) for __ in xrange(no_particles)]
-        self.best_position = None
+    def __init__(self, no_params, no_particles, no_groups=1, inertia=1.2, cognitive_comp=2, social_comp=2): # Shi & Eberhart 1998
+        assert int(no_particles / no_groups) == float(no_particles) / no_groups
+        self.particle_groups = [[Particle(no_params, inertia, cognitive_comp, social_comp)
+                                 for group_members in
+                                 xrange(no_particles / no_groups)
+                                 ]
+                                for groups in
+                                xrange(no_groups)
+                                ]
+        self.particles = [particle for group in self.particle_groups for particle in group]
+        self.best_overall_position_coords = None
+        self.best_group_positions = [None for group in self.particle_groups]
 
-    def get_best_position(self, fitness_function):
-        positions = (particle.best_position(fitness_function) for particle in self.particles)
-        return max(positions, key=operator.attrgetter('fitness')).position
+    def get_best_position_coords(self, fitness_function, particles=None):
+        return self._get_best_position(fitness_function, particles=particles).position
+
+    def _get_best_position(self, fitness_function, particles=None):
+        particles = self.particles if particles is None else particles
+        positions = (particle.best_position(fitness_function) for particle in particles)
+        return max(positions, key=operator.attrgetter('fitness'))
 
     def _set_best_position(self, position):
-        self.best_position = position
+        self.best_overall_position_coords = position
 
     def step(self, fitness_function):
-        if self.best_position is None:
-           self.best_position = self.get_best_position(fitness_function)
+        if self.best_overall_position_coords is None:
+            self.best_group_positions = [self._get_best_position(fitness_function, particles=group)
+                                         for group in
+                                         self.particle_groups
+                                         ]
 
-        [particle.update(self.best_position) for particle in self.particles]
-        self.best_position = self.get_best_position(fitness_function)
+            self.best_overall_position_coords = max(self.best_group_positions, key=operator.attrgetter('fitness'))
+
+        [particle.update(best_group_position.position)
+         for best_group_position, group in
+         itertools.izip(self.best_group_positions, self.particle_groups)
+         for particle in
+         group
+         ]
+
+        self.best_group_positions = [self._get_best_position(fitness_function, particles=group)
+                                     for group in
+                                     self.particle_groups
+                                     ]
+
+        self.best_overall_position_coords = max(self.best_group_positions, key=operator.attrgetter('fitness'))
 
         return self.particles
 
@@ -83,7 +116,7 @@ class Swarm(object):
 
         max_iterations = xrange(max_iterations) if max_iterations is not None else itertools.count()
         termination_function = termination_function if termination_function is not None else lambda x: False
-        
+
         for i in max_iterations:
             yield self.step(fitness_function)
             if termination_function(self):

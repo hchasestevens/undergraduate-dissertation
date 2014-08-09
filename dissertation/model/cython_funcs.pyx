2f7d394403ac640b82c1d557c00564a80afd0479
H. Chase Stevens <chase@chasestevens.com>
2014-08-09 15:30:31 +0100

    cython_funcs - use spaces for indentation

--- dissertation/model/cython_funcs.pyx
+++ dissertation/model/cython_funcs.pyx
@@ -2,18 +2,20 @@
 
 
 def comm_success(
-	int cost,
-	int ambiguous_reference_cost,
-	int success_points,
+    int cost,
+    int ambiguous_reference_cost,
+    int success_points,
     double player_ambiguity_probability,
-	double partner_ambiguity_probability,
-	double partner_sum):
-
-	if partner_ambiguity_probability > 0.:
-		return ((ambiguous_reference_cost * player_ambiguity_probability) +  # Times using ambiguous term
-				(success_points * player_ambiguity_probability * (partner_ambiguity_probability / partner_sum)) +  # Times ambiguous term is understood
-				((cost + success_points) * (1 - player_ambiguity_probability))  # Times using unambiguous term (always understood)
-				)
-	return ((ambiguous_reference_cost * player_ambiguity_probability) +  # Times using ambiguous term (cost only - partner unreceptive)
-			((cost + success_points) * (1 - player_ambiguity_probability))  # Times using unambiguous term
-			)
+    double partner_ambiguity_probability,
+    double partner_sum):
+    
+    if partner_ambiguity_probability > 0.:
+        return (
+            (ambiguous_reference_cost * player_ambiguity_probability) +  # Times using ambiguous term
+            (success_points * player_ambiguity_probability * (partner_ambiguity_probability / partner_sum)) +  # Times ambiguous term is understood
+            ((cost + success_points) * (1 - player_ambiguity_probability))  # Times using unambiguous term (always understood)
+        )
+    return (
+        (ambiguous_reference_cost * player_ambiguity_probability) +  # Times using ambiguous term (cost only - partner unreceptive)
+        ((cost + success_points) * (1 - player_ambiguity_probability))  # Times using unambiguous term
+    )

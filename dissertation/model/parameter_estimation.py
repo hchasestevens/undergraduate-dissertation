0fc91dcf1b7cce82813942bdc6a41c4ed67e8b70
H. Chase Stevens <chase@chasestevens.com>
2014-08-14 21:19:31 +0100

    parameter_estimation - finalize fitness function, add Experiment class - Issue #9, #4

--- dissertation/model/parameter_estimation.py
+++ dissertation/model/parameter_estimation.py
@@ -3,32 +3,89 @@
 import itertools
 import numpy
 import random
+from collections import namedtuple, defaultdict
+import pyximport; pyximport.install()
 
 import utils
 from game import communication_scenario_factory, game_factory
 from swarm import Swarm
+from cython_funcs import comm_success
 
 
-ROHDE_GAMES = map(game_factory, (
-    communication_scenario_factory(
-        reference_costs=costs,
-        ambiguous_reference_cost=-80.,
-        success_points=85.,
-    )
-    for costs in (
-        (-60., -120., -280.),  # Flowers
-        (-60., -120., -250.),  # Trees
-        (-80., -140., -165.),  # Flowers (similar cost)
-        (-80., -135., -170.),  # Trees (similar cost)
+class Experiment(namedtuple('Experiment', 'settings results')):
+    """Container for experiment settings and results."""
+    
+    Settings = namedtuple('Settings', 'reference_costs, ambiguous_reference_cost, success_points')
+    CoordinationResults = namedtuple('CoordinationResults', 'ambiguous unambiguous none')
+
+    @property
+    def game(self):
+        return game_factory(
+            communication_scenario_factory(
+                **self.settings._asdict()
+            )
+        )
+
+    def difference(self, other_results):
+        return sum(
+            abs(getattr(self.results, field) - getattr(other_results, field)) ** 2
+            for field in
+            self.results._fields
+        )
+
+
+COORDINATED_COMM_THRESHOLD = 0.95
+
+ROHDE_EXPERIMENTS = frozenset((
+    Experiment(  # Flowers
+        Experiment.Settings(
+            reference_costs=(-60., -120., -280.),
+            ambiguous_reference_cost=-80.,
+            success_points=85.,
+        ), 
+        Experiment.CoordinationResults(
+            ambiguous=0.5,
+            unambiguous=0.1,
+            none=0.4,
+        )
+    ),
+    Experiment(  # Trees
+        Experiment.Settings(
+            reference_costs=(-60., -120., -250.),
+            ambiguous_reference_cost=-80.,
+            success_points=85.,
+        ), 
+        Experiment.CoordinationResults(
+            ambiguous=0.5,
+            unambiguous=0.1,
+            none=0.4,
+        )
+    ),
+    Experiment(  # Flowers (similar cost)
+        Experiment.Settings(
+            reference_costs=(-80., -140., -165.),
+            ambiguous_reference_cost=-80.,
+            success_points=85.,
+        ),
+        Experiment.CoordinationResults(
+            ambiguous=0.6,
+            unambiguous=0.0,
+            none=0.4,
+        )
+    ),
+    Experiment(  # Trees (similar cost)
+        Experiment.Settings(
+            reference_costs=(-80., -135., -170.),
+            ambiguous_reference_cost=-80.,
+            success_points=85.,
+        ), 
+        Experiment.CoordinationResults(
+            ambigous=0.8,
+            unambiguous=0.0,
+            none=0.2,
+        )
     )
 ))
-
-ROHDE_GAME_RESULTS = (  # Re-read Rohde et al. and double-check these
-    0.5,  # Flowers
-    0.5,  # Trees
-    0.8,  # Flowers (similar cost)
-    0.8,  # Trees (similar cost)
-)
 
 
 def get_parameters(particle_position):
@@ -53,9 +110,35 @@ def get_parameters(particle_position):
     return iterations, particle_settings
 
 
-def compare(final_groups, expected_results):
-    """Characterize and compare final status of groups vs. those expected."""
-    return -1
+def get_coordination_results(final_groups):
+    """
+    Characterize final status of groups into Experiment.CoordinationResults
+    object.
+    """
+    descriptions = defaultdict(float)
+
+    num_groups = float(len(groups))
+    for p1, p2 in groups:
+        overall_success = 0.
+        possible_success = 0.
+        p1_sum = sum(p1)
+        p2_sum = sum(p2)
+        
+        for p1_prob, p2_prob in itertools.izip(p1, p2):
+            overall_success += comm_success(0, 0, 1, p1_prob, p2_prob, p2_sum)
+            overall_success += comm_success(0, 0, 1, p2_prob, p1_prob, p1_sum)
+            possible_success += 2.
+
+        if (overall_success / possible_success) <= COORDINATED_COMM_THRESHOLD:
+            description = 'none'
+        elif p1_sum > abs(p1_sum - (1. / 3.)):
+            description = 'ambiguous'
+        else:
+            description = 'unambiguous'
+
+        descriptions[description] += 1. / num_groups
+
+    return Experiment.CoordinationResults(**descriptions)
 
 
 def fitness(particle_position):
@@ -74,13 +157,14 @@ def fitness(particle_position):
 
     scores = []
     # run psos with these params
-    for game, expected_results in itertools.izip(ROHDE_GAMES, ROHDE_GAME_RESULTS):
+    for experiment in ROHDE_EXPERIMENTS:
         swarm = Swarm(dimensions, group_size, no_groups, **particle_settings)
-        for groups in swarm.step_until(game, max_iterations=iterations, return_groups=True):
+        for groups in swarm.step_until(experiment.game, max_iterations=iterations, return_groups=True):
             pass
-        scores.append(compare(groups, expected_results))
+        scores.append(experiment.difference(get_coordination_results(groups)))
 
     return sum(scores) / float(len(scores))
+
 
 def mitchell_sampling_factory(no_dimensions, no_candidates=10):
     """Create sample generator using Mitchell 1991 algorithm."""

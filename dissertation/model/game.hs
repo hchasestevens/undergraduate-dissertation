f37ce19b56f9a0cb892dd98ea223bc6b0b536bda
H. Chase Stevens <chase@chasestevens.com>
2014-09-03 04:21:35 +0100

    game, swarm, main - pep-8 (whitespace, trailing newline) - Issue #15

--- dissertation/model/game.hs
+++ dissertation/model/game.hs
@@ -3,31 +3,34 @@ import Data.List
 
 single_comm_success :: Float -> Float -> Float -> Float -> Float -> Float -> Float
 single_comm_success ambiguous_reference_cost success_points partner_sum cost player_prob partner_prob
-	| partner_prob > 0.0 = ambiguous_cost + ambiguous_reward + unambiguous_points
-	| otherwise = ambiguous_cost + unambiguous_points
-	where
-		ambiguous_cost = ambiguous_reference_cost * player_prob
-		ambiguous_reward = success_points * player_prob * (partner_prob / partner_sum)
-		unambiguous_points = ((cost + success_points) * (1.0 - player_prob))
+    | partner_prob > 0.0 = ambiguous_cost + ambiguous_reward + unambiguous_points
+    | otherwise = ambiguous_cost + unambiguous_points
+    where
+        ambiguous_cost = ambiguous_reference_cost * player_prob
+        ambiguous_reward = success_points * player_prob * (partner_prob / partner_sum)
+        unambiguous_points = ((cost + success_points) * (1.0 - player_prob))
+
 
 comm_success :: [Float] -> Float -> Float -> [Float] -> [Float] -> Float
 comm_success costs ambiguous_reference_cost success_points player partner =
-	sum $ zipWith3 item_comm_success costs player partner
-	where
-		partner_sum = sum partner
-		item_comm_success = single_comm_success ambiguous_reference_cost success_points partner_sum
+    sum $ zipWith3 item_comm_success costs player partner
+    where
+        partner_sum = sum partner
+        item_comm_success = single_comm_success ambiguous_reference_cost success_points partner_sum
+
 
 game :: ([Float] -> [Float] -> Float) -> [Float] -> [[Float]] -> Float -- Maybe have this take particles, have position implement eq?
 game comm_func player [] = 0.0
 game comm_func player group = sum $ map (pair_success player) $ delete player group
     where
-    	pair_success player partner = comm_func player partner + comm_func partner player
+        pair_success player partner = comm_func player partner + comm_func partner player
+
 
 main = do
-	putStrLn $ show $ comm_success [0] 0 1 [1] [1]
-	putStrLn $ show $ comm_success [0] 0 1 [1] [0]
-	putStrLn $ show $ comm_success [0] 0 1 [1] [0.5]
-	putStrLn $ show $ comm_success [0] 0 1 [1] [1]
-	putStrLn $ show $ comm_success [0] 0 1 [0] [1]
-	putStrLn $ show $ comm_success [0] 0 1 [0.5] [1]
-	return $ (game $ comm_success [0] 0 1) [1] [[0.99], [0], [0.5]]
+    putStrLn $ show $ comm_success [0] 0 1 [1] [1]
+    putStrLn $ show $ comm_success [0] 0 1 [1] [0]
+    putStrLn $ show $ comm_success [0] 0 1 [1] [0.5]
+    putStrLn $ show $ comm_success [0] 0 1 [1] [1]
+    putStrLn $ show $ comm_success [0] 0 1 [0] [1]
+    putStrLn $ show $ comm_success [0] 0 1 [0.5] [1]
+    return $ (game $ comm_success [0] 0 1) [1] [[0.99], [0], [0.5]]
--- dissertation/model/swarm.hs
+++ dissertation/model/swarm.hs
@@ -14,8 +14,8 @@ vector |* scalar = map (* scalar) vector
 
 
 data Position = Position {
-	point :: [Float],
-	fitness :: Maybe Float
+    point :: [Float],
+    fitness :: Maybe Float
 } deriving (Show)
 
 make_position :: [Float] -> Maybe ([Float] -> Float) -> Position
@@ -24,107 +24,107 @@ make_position point _ = Position {point=point, fitness=Nothing}
 
 
 data ParticleConfig = ParticleConfig {
-	max_velocity :: Float,
-	max_position :: Float,
-	min_position :: Float,
-	
-	respect_boundaries :: Bool,
+    max_velocity :: Float,
+    max_position :: Float,
+    min_position :: Float,
+    
+    respect_boundaries :: Bool,
 
-	initial_inertia :: Float,
-	cognitive_comp :: Float,
-	social_comp :: Float,
+    initial_inertia :: Float,
+    cognitive_comp :: Float,
+    social_comp :: Float,
 
-	inertial_dampening :: Float,
-	velocity_dampening :: Float
+    inertial_dampening :: Float,
+    velocity_dampening :: Float
 } deriving (Show)
 
 defaultParticleConfig = ParticleConfig {
-	max_velocity=1, 
-	max_position=10,
-	min_position=(-10),
+    max_velocity=1, 
+    max_position=10,
+    min_position=(-10),
 
-	respect_boundaries=False,
+    respect_boundaries=False,
 
-	-- Values taken from Shi & Eberhart 1998
-	initial_inertia=1.2,
-	cognitive_comp=2,
-	social_comp=2,
+    -- Values taken from Shi & Eberhart 1998
+    initial_inertia=1.2,
+    cognitive_comp=2,
+    social_comp=2,
 
-	inertial_dampening=1.001,
-	velocity_dampening=1
+    inertial_dampening=1.001,
+    velocity_dampening=1
 }
 
 
 data Particle = Particle {
-	uuid :: Int,
-	
-	config :: ParticleConfig,
+    uuid :: Int,
+    
+    config :: ParticleConfig,
 
-	position :: Position,
-	velocity :: [Float],
-	best_position :: Position,
+    position :: Position,
+    velocity :: [Float],
+    best_position :: Position,
 
-	time :: Int
+    time :: Int
 } deriving (Show)
 
 new_particle :: Int -> ParticleConfig -> Position -> [Float] -> Particle
 new_particle id_num conf pos vel = 
-	Particle {
-		uuid=id_num, 
-		config=conf, 
-		position=pos, 
-		velocity=vel,
-		best_position=pos,
-		time=0
-	}
+    Particle {
+        uuid=id_num, 
+        config=conf, 
+        position=pos, 
+        velocity=vel,
+        best_position=pos,
+        time=0
+    }
 
 current_inertia :: Particle -> Float
 current_inertia particle = initial_inertia conf / inertial_dampening conf ^ time particle
     where
-    	conf = config particle
+        conf = config particle
 
 clip :: (Ord a) => a -> a -> [a] -> [a]
 clip min_value max_value = map ((max min_value) . (min max_value))
 
 update :: Position -> ([Float] -> Float) -> Particle -> Float -> Float -> Particle
 update best_neighbor_position fitness_func particle cognitive_mod social_mod =
-	particle {
-		position=position',
-		velocity=velocity',
-		best_position=best_position',
-		time=time particle + 1
-	}
-	where
-		conf = config particle
-		v_max = max_velocity conf
-		v_min = -v_max
-		current_point = point . position $ particle
+    particle {
+        position=position',
+        velocity=velocity',
+        best_position=best_position',
+        time=time particle + 1
+    }
+    where
+        conf = config particle
+        v_max = max_velocity conf
+        v_min = -v_max
+        current_point = point . position $ particle
 
-		-- new velocity:
-		inertia = current_inertia particle
+        -- new velocity:
+        inertia = current_inertia particle
 
-		inertial_velocity = velocity particle |* inertia
-		cognitive_velocity = (point . best_position) particle |-| current_point |* (cognitive_comp conf) |* cognitive_mod
-		social_velocity = (point best_neighbor_position) |-| current_point |* (social_comp conf) |* social_mod
+        inertial_velocity = velocity particle |* inertia
+        cognitive_velocity = (point . best_position) particle |-| current_point |* (cognitive_comp conf) |* cognitive_mod
+        social_velocity = (point best_neighbor_position) |-| current_point |* (social_comp conf) |* social_mod
 
-		velocity' = clip v_min v_max $ inertial_velocity |+| cognitive_velocity |+| social_velocity
-		
-		-- new position:
-		(p_max, p_min) = if (not . respect_boundaries) conf 
-							then (max_position conf, min_position conf)
-							else (1.0, 0.0)
-		point' = clip p_max p_min $ velocity' |* (velocity_dampening conf) |+| current_point
-		fitness_func' = if all (<= p_max) point' && all (>= p_min) point'  -- Technique from Engelbrecht 2005 
-						   then Just fitness_func 
-						   else Nothing
-		position' = make_position point' fitness_func'
+        velocity' = clip v_min v_max $ inertial_velocity |+| cognitive_velocity |+| social_velocity
+        
+        -- new position:
+        (p_max, p_min) = if (not . respect_boundaries) conf 
+                            then (max_position conf, min_position conf)
+                            else (1.0, 0.0)
+        point' = clip p_max p_min $ velocity' |* (velocity_dampening conf) |+| current_point
+        fitness_func' = if all (<= p_max) point' && all (>= p_min) point'  -- Technique from Engelbrecht 2005 
+                           then Just fitness_func 
+                           else Nothing
+        position' = make_position point' fitness_func'
 
-		-- check for new best position (n.b. order matters):
-		best_position' = maximumBy (compare `on` fitness) [position', best_position particle]
+        -- check for new best position (n.b. order matters):
+        best_position' = maximumBy (compare `on` fitness) [position', best_position particle]
 
 
 main = do
-	print $ [1..5] |* 2.0
-	print $ [1..5] |+| [1..5]
-	print $ [1..5] |-| [1..5]
-	return ()
+    print $ [1..5] |* 2.0
+    print $ [1..5] |+| [1..5]
+    print $ [1..5] |-| [1..5]
+    return ()
